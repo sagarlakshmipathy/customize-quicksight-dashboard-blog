0956aacb1a81896400291de8843262cf04281aca
aws-sagar <yolavidhya@amazon.com>
2023-01-26 19:29:11 +0000

    prettify

--- dashboardCustomization.py
+++ dashboardCustomization.py
@@ -6,35 +6,6 @@ import boto3
 import json
 import time
 
-# initialize quicksight
-qs_client = boto3.client('quicksight')
-
-# initialize the variables
-aws_account_id = '733585711144'
-region = 'us-east-1'
-dashboard_id = 'd280fdec-2adf-45bb-813d-cceece251ec3'
-target_dataset_id = 'b40f8cfd-adb1-40c4-b972-b4a0289d7a56'
-target_dataset_identifier = 'Strategic'
-target_dataset_arn = f'arn:aws:quicksight:{region}:{aws_account_id}:dataset/{target_dataset_id}'
-sheet_id = 'd280fdec-2adf-45bb-813d-cceece251ec3_642ba41e-f491-4357-bf4a-a2d4eb9638bc'
-visual_id = 'd280fdec-2adf-45bb-813d-cceece251ec3_52d335ed-bbbc-46bd-98d6-000c81a214e6'
-replacement_url = 'https://tiny.amazon.com/1dz1oyrsy/aacbs3useaamazawspng'
-
-# target dashboard parameters
-target_dashboard_id = 'StrategicDashboard'
-target_dashboard_name = 'Strategic'
-
-# describe dashbboard
-dashboard_definition = qs_client.describe_dashboard_definition(AwsAccountId=aws_account_id, DashboardId=dashboard_id)
-
-# Optional: download definition
-with open('./enterpriseDashboard.json', "w") as outfile:
-    json.dump(dashboard_definition, outfile, indent=4)
-
-# cleanup residue
-# for key in ['Status', 'Name', 'DashboardId', 'RequestId', 'ResponseMetadata']:
-#     dashboard_definition.pop(key)
-
 def update_nested_dict(in_dict, key, value):
     """Replaces the existing value of the key with a new value
 
@@ -56,6 +27,31 @@ def update_nested_dict(in_dict, key, value):
                 if isinstance(o, dict):
                     update_nested_dict(o, key, value)
 
+# initialize quicksight
+qs_client = boto3.client('quicksight')
+
+# initialize the variables
+aws_account_id = '733585711144'
+region = 'us-east-1'
+dashboard_id = 'd280fdec-2adf-45bb-813d-cceece251ec3'
+target_dataset_id = 'b40f8cfd-adb1-40c4-b972-b4a0289d7a56'
+target_dataset_identifier = 'Strategic'
+target_dataset_arn = f'arn:aws:quicksight:{region}:{aws_account_id}:dataset/{target_dataset_id}'
+sheet_id = 'd280fdec-2adf-45bb-813d-cceece251ec3_642ba41e-f491-4357-bf4a-a2d4eb9638bc'
+visual_id = 'd280fdec-2adf-45bb-813d-cceece251ec3_52d335ed-bbbc-46bd-98d6-000c81a214e6'
+replacement_url = 'https://tiny.amazon.com/1dz1oyrsy/aacbs3useaamazawspng'
+
+# user defined target dashboard parameters
+target_dashboard_id = 'StrategicDashboard'
+target_dashboard_name = 'Strategic'
+
+# describe dashboard definition
+dashboard_definition = qs_client.describe_dashboard_definition(AwsAccountId=aws_account_id, DashboardId=dashboard_id)
+
+# Optional: download dashboard definition
+with open('./enterpriseDashboard.json', "w") as outfile:
+    json.dump(dashboard_definition, outfile, indent=4)
+
 # creating object dictionaries
 dataset_dict = dashboard_definition['Definition']['DataSetIdentifierDeclarations'][0]
 sheets_dict = dashboard_definition['Definition']['Sheets']
@@ -69,7 +65,7 @@ update_nested_dict(dataset_dict, 'Identifier', target_dataset_identifier)
 update_nested_dict(dataset_dict, 'DataSetArn', target_dataset_arn)
 update_nested_dict(dashboard_definition, 'DataSetIdentifier', target_dataset_identifier)
 
-# update visual URL in your JSON
+# update visual URL in target definition
 for visual in visuals_in_sheet:
     try:
        if visual['CustomContentVisual']['VisualId'] == visual_id:
@@ -78,7 +74,7 @@ for visual in visuals_in_sheet:
     except KeyError:
         continue
 
-# Optional: store the resultant json locally
+# Optional: download the target definition
 with open('./modifiedJson.json', 'w') as outfile:
     json.dump(dashboard_definition, outfile, indent=4)
 
